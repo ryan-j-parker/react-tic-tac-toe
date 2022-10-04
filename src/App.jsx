9aafd441693bc7c676f6c41354c28e82986581c4
Ryan J. Parker <ryan.jos.parker@gmail.com>
2022-10-04 12:20:47 -0700

    add message for each player's turn

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
+import { useContext } from 'react';
 import './App.css';
 import Board from './components/Board/Board';
+import { GameContext } from './context/GameContext';
 
 function App() {
+  const { message } = useContext(GameContext);
+
   return (
     <>
       <div className="App">
         <div className="title">
           <h1>Alchemy Tic-Tac-Toe</h1>
-          <h5>(for Americans)</h5>
+          <h5>{message}</h5>
         </div>
         <Board />
       </div>
--- src/context/GameContext.jsx
+++ src/context/GameContext.jsx
@@ -7,8 +7,8 @@ const GameContext = createContext();
 
 const GameProvider = ({ children }) => {
   const [board, setBoard] = useState(boxes);
-
   const [player, setPlayer] = useState('X');
+  const [message, setMessage] = useState('');
 
   const setSpace = (id) => {
     setBoard((prevBoard) =>
@@ -16,13 +16,24 @@ const GameProvider = ({ children }) => {
     );
 
     setPlayer(player === 'X' ? 'O' : 'X');
+    setMessage(`Your turn, ${player}`);
   };
+
+  //   const message = (player) => {
+  //     if (player === 'X') {
+  //       return 'Your turn, X';
+  //     } else if (player === 'O') {
+  //       return 'Your turn, O';
+  //     }
+  //   };
 
   return (
     <GameContext.Provider
       value={{
         board,
         setSpace,
+        player,
+        message,
       }}
     >
       {children}
